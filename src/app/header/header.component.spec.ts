e8da97afa5c73a72fadac2180f488ef12f10326b
van <van@hovercloud.com>
2016-11-16 11:22:51 -0800

    added html for a simple navbar and a footer, added a simple unit test for header content

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -4,8 +4,24 @@ import { TestBed, async } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 
 describe('Component: Header', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        HeaderComponent
+      ],
+    });
+  });
+
   it('should create an instance', () => {
-    let component = new HeaderComponent();
+    let fixture = TestBed.createComponent(HeaderComponent);
+    let component = fixture.debugElement.componentInstance;
     expect(component).toBeTruthy();
   });
+
+  it('should have top-left navbar brand name "Calendar"', async(() => {
+    let fixture = TestBed.createComponent(HeaderComponent);
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.navbar-brand').textContent).toContain('Calendar');
+  }));
 });
